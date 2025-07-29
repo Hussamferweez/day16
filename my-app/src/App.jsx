3798f54ab7b725d029f1f57c6eef11b979005086
hussamayman <hussamaymanfz@gmail.com>
2025-07-29 04:58:37 +0300

    day17

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import About from './components/About/About'
+import Home from './components/Home/Home'
+import Contact from './components/Contact/Contact';
+import { useState } from 'react';
+import Parent from './components/parent/Parent';
+
+//parent component
 function App() {
-  const [count, setCount] = useState(0)
+
+const {counter , setCounter} = useState("hamada")
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Parent />
+      <Home/>
+      <About name = {"counter"} age = {30}/>
+      <Contact/>
     </>
+
+
+  /* {parent child => comp jsx} */
+
+  /* {render comp in another comp} */
+ 
+  /*
+    1- use the function navbar one in home , about and contact
+    2- share data about two component
+    
+    Home 1
+    notification 1
+    profile 1
+    settings 1
+
+    calling 4
+
+
+    send data => about 2 comp => parent and child
+
+    app => calling 1
+    home 
+    not
+    prof
+    settings
+
+
+    how send data about 2 component => parnet to child  PROPS
+    attribute comp => like html
+
+  */
+
+
+
   )
 }
 

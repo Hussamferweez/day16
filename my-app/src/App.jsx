18e4893d4a768176076f3a8a122f9fe30f9f65bd
hussamayman <hussamaymanfz@gmail.com>
2025-07-30 05:59:29 +0300

    day18

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,57 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import NotFound from "./Pages/NotFound";
+// import HomeNested from "./pages/HomeNested";
 
-import About from './components/About/About'
-import Home from './components/Home/Home'
-import Contact from './components/Contact/Contact';
-import { useState } from 'react';
-import Parent from './components/parent/Parent';
-
-//parent component
 function App() {
-
-const {counter , setCounter} = useState("hamada")
-
   return (
-    <>
-      <Parent />
-      <Home/>
-      <About name = {"counter"} age = {30}/>
-      <Contact/>
-    </>
-
-
-  /* {parent child => comp jsx} */
-
-  /* {render comp in another comp} */
- 
-  /*
-    1- use the function navbar one in home , about and contact
-    2- share data about two component
-    
-    Home 1
-    notification 1
-    profile 1
-    settings 1
-
-    calling 4
-
-
-    send data => about 2 comp => parent and child
-
-    app => calling 1
-    home 
-    not
-    prof
-    settings
-
-
-    how send data about 2 component => parnet to child  PROPS
-    attribute comp => like html
-
-  */
-
-
-
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />}>
+          <Route path="counter" element={<HomeNested />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,14 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import { createRoot } from 'react-dom/client'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.js"
-import "./../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
-import './index.css'
-import App from './App.jsx'
-
-//file js => package / node
-//component => comp => main
-
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-)
+  </React.StrictMode>
+);
